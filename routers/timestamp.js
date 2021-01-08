9c617e40ed5a516d7ffac9ec6ec7608c1bf6e745
Viviño <Vivinh0@users.noreply.github.com>
2021-01-08 13:18:40 +0100

    Add middleware checks invalid date

--- routers/timestamp.js
+++ routers/timestamp.js
@@ -12,15 +12,27 @@ router
     const response = { unix: date, utc: dateInUTC };
     res.json(response);
   })
-  .get("/:date", (req, res, next) => {
-    //   res.json({ msg: "Ok" });
-    const inputDate = new Date(req.params.date);
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getTime
-    const dateInMilliseconds = inputDate.getTime();
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toUTCString
-    const dateInUTC = inputDate.toUTCString();
-    const response = { unix: dateInMilliseconds, utc: dateInUTC };
-    res.json(response);
-  });
+  .get(
+    "/:date",
+    // Middleware checks if input date is invalid
+    (req, res, next) => {
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
+      if (Date.parse(req.params.date)) {
+        next();
+      } else {
+        res.json({ error: "Invalid Date" });
+      }
+    },
+    (req, res, next) => {
+      //   res.json({ msg: "Ok" });
+      const inputDate = new Date(req.params.date);
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getTime
+      const dateInMilliseconds = inputDate.getTime();
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toUTCString
+      const dateInUTC = inputDate.toUTCString();
+      const response = { unix: dateInMilliseconds, utc: dateInUTC };
+      res.json(response);
+    }
+  );
 
 module.exports = router;
