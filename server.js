4c62d1ba514f0b25bfd2bbe184c39e881a0fd38a
Viviño <Vivinh0@users.noreply.github.com>
2021-01-08 16:20:09 +0100

    Disable start up server log on test env

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use("/api/timestamp", require("./routers/timestamp"));
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 5000, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+  if (RegExp('[dev | prod]').test(process.env.NODE_ENV)) {
+    console.log("Your app is listening on port " + listener.address().port);
+  }
 });
 
 module.exports = listener;
